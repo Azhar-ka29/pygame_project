64dfce7cad05882e9e019ed71618006bbd91432a
Azhar-ka29 <azhar.kabdygali@fizmat.kz>
2022-01-14 20:17:32 +0600

    class Computer

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ class Ball(Block):
             start_counter = 1
         if (cur_time - self.time) >= 2100:
             self.active = True
-        time_counter = font.render(str(start_counter), True, black_color)
+        time_counter = font.render(str(start_counter), True, grey_color)
         time_counter_rect = time_counter.get_rect(center=(width / 2, height / 2 + 50))
         pygame.draw.rect(screen, bg_color, time_counter_rect)
         screen.blit(time_counter, time_counter_rect)
@@ -94,19 +94,39 @@ class Ball(Block):
             self.rect.x += self.speed_x
             self.rect.y += self.speed_y
             self.actions()
+
         else:
             self.counter()
+
+
+class Computer(Block):
+    def __init__(self, path, x, y, speed):
+        super().__init__(path, x, y)
+        self.speed = speed
+
+    def screen_constrain(self):
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= height:
+            self.rect.bottom = height
+
+    def update(self, ball_group):
+        if self.rect.top < ball_group.sprite.rect.y:
+            self.rect.y += self.speed
+        if self.rect.bottom > ball_group.sprite.rect.y:
+            self.rect.y -= self.speed
+        self.screen_constrain()
 
 
 pygame.init()
 clock = pygame.time.Clock()
 
-size = width, height = 600, 600
+size = width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Ping Pong')
 
 bg_color = pygame.Color('#2F373F')
-black_color = pygame.Color('black')
+grey_color = pygame.Color('grey')
 font = pygame.font.Font('freesansbold.ttf', 35)
 middle_strip = pygame.Rect(width / 2 - 2, 0, 4, height)
 
@@ -117,8 +137,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(bg_color)
-    pygame.draw.rect(screen, black_color, middle_strip)
+    screen.fill(pygame.Color('white'))
+    pygame.draw.rect(screen, grey_color, middle_strip)
+
     pygame.display.flip()
     clock.tick(120)
 
